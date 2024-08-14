87aa84d323aa484dd4e2d85fcbcf22ff371a8d11
ChaosAIVision <truongnn20022003@gmail.com>
2024-08-14 23:27:54 +0700

    test inferences

--- detect.py
+++ detect.py
@@ -1,22 +1,104 @@
-import pickle
-import os
-def load_cache(cache_file_path):
-    """
-    Load cache from a Pickle file.
+from models.yolo import Yolov1
+import torch
+from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Resize, Normalize
+from utils.general import ManagerDataYaml, ManageSaveDir, cellboxes_to_boxes, convert_xywh2xyxy
+from utils.metric import  non_max_suppression
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
 
-    Args:
-        cache_file_path (str): Path to the cache file.
 
-    Returns:
-        list: List of cached data loaded from the file.
-    """
-    if not os.path.isfile(cache_file_path):
-        raise FileNotFoundError(f"The cache file {cache_file_path} does not exist.")
+
+#Load model
+S = 7
+B = 2
+C = 20
+model = Yolov1(split_size=S, num_boxes=B, num_classes=C)
+pretrain_weight = '/home/chaos/Documents/ChaosAIVision/yolo_output/result3/weights/best.pt'
+
+checkpoint = torch.load(pretrain_weight)
+model.load_state_dict(checkpoint['model_state_dict'])
+
+
+#Load image
+image = Image.open('/home/chaos/Documents/ChaosAIVision/dataset/dataset/images/000164.jpg')
+classes =  ["aeroplane",
+"bicycle",
+"bird",
+"boat",
+"bottle",
+"bus",
+"car",
+"cat",
+"chair",
+"cow",
+"diningtable",
+"dog",
+"horse",
+"motorbike",
+"person",
+"pottedplant",
+"sheep",
+"sofa",
+"train",
+"tvmonitor"]
+
+
+transform = Compose([
+                ToTensor(),
+                Resize((448,448)),
+                Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                ])
+DEVICE = "cuda" if torch.cuda.is_available else "cpu"
+model.to(DEVICE)
+image_transform = transform(image)
+image_transform = image_transform.unsqueeze(0)
+output = model(image_transform.to(DEVICE))
+pred_bboxes = cellboxes_to_boxes(output)
+all_pred_boxes = []
+batch_size = output.shape[0]
+iou_threshold =0.4
+conf = 0.40
+box_format = 'midpoint'
+for idx in range(batch_size):
+                    nms_boxes = non_max_suppression(pred_bboxes[idx],
+                                                    iou_threshold,
+                                                    conf, 
+                                                    box_format)
+                    for nms_box in nms_boxes:
+                        all_pred_boxes.append(nms_box)
+
+
+
+#show image
+image_np = np.array(image)
+# Khởi tạo hình vẽ
+fig, ax = plt.subplots(1)
+
+# Hiển thị ảnh
+ax.imshow(image_np)
+
+# Thêm bounding boxes
+for bbox in all_pred_boxes:
+    class_id, conf, x_center, y_center, width, height = bbox
     
-    with open(cache_file_path, 'rb') as f:
-        cache = pickle.load(f)
+    # Chuyển đổi tọa độ YOLO (trung tâm và kích thước) sang (x_min, y_min, width, height)
+    x_center *= image_np.shape[1]
+    y_center *= image_np.shape[0]
+    width *= image_np.shape[1]
+    height *= image_np.shape[0]
     
-    return cache
-path = '/home/chaos/Documents/ChaosAIVision/dataset/voc_dataset/train/labels.cache'
-a = load_cache(path)
-print(a)
+    x_min = int(x_center - width / 2)
+    y_min = int(y_center - height / 2)
+    
+    # Tạo hình chữ nhật (bounding box)
+    rect = patches.Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+    
+    # In thông tin lớp và độ tin cậy
+    plt.text(x_min, y_min, f'Class: {classes[int(class_id)]} Conf: {conf:.2f}', color='red', fontsize=12, weight='bold', bbox=dict(facecolor='white', alpha=0.7))
+
+# Hiển thị kết quả
+plt.axis('off')  # Tắt trục
+plt.show()
